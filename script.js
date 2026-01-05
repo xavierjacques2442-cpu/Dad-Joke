922282e9fb226f806b62ef2ba8f6e27a8eca51fa
x24USER <xavierjacques2442@gmail.com>
2026-01-05 15:58:50 -0800

    script/peer review

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+ //Xavier 
+ //1/5/2026
+ //Fetch a random joke from an API and display it on the webpage when a button is clicked
+ //Peer Review:
+ 
  const jokeDiv = document.getElementById("joke");
     const btn = document.getElementById("getJoke");
 
